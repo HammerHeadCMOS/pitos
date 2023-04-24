8910c1f3ba5b3c12a2e83128256ced7b2df2a12e
LABORATORIO FELIPE SEGOVIA <laboratorio.fs@ad.uisek.edu.ec>
2023-04-24 10:29:36 -0500

    version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Tarjeta{
+  titulo:string,
+  subtitulo:string,
+  image:string,
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app1';
+export class AppComponent implements OnInit{
+  title = 'The XD momento';
+  public ArregloTarjetas:Tarjeta[]=[]
+  ngOnInit(): void {
+    this.ArregloTarjetas=[
+      {titulo:"Video 1", subtitulo:"Subtitulo video 1", image:"https://pbs.twimg.com/media/EaLV_tKWsAUWNpB?format=jpg&name=small"},
+      {titulo:"Video 2", subtitulo:"Subtitulo video 2", image:"https://media.tenor.com/80ucPA_YI6cAAAAC/xddddddd.gif"},
+      {titulo:"Video 3", subtitulo:"Subtitulo video 3", image:"https://media.tenor.com/thkmNH1mKXoAAAAd/trollface-xddddddd.gif"},
+      {titulo:"Video 3", subtitulo:"Subtitulo video 3", image:"https://cdntube.b-cdn.net/thumbnails/aa3faafb9722161dca3b6bb16a0f3fe0069a33b0.jpg"},
+    ] 
+  }
 }
+
+
